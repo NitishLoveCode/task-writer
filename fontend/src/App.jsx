75cbc62d8e79a0b67659c3e17bc01a1671b75578
Manish kumar <manishlovecode@gmail.com>
2024-03-03 19:23:42 +0530

    changes made in auth at home page

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -6,12 +6,34 @@ import SignUp from "./component/SignUp";
 import Home from "./component/Home";
 
 const App = () => {
+  
   return (
     <div>
       <Routes>
-        <Route path={"/"} element={<Layout><Home/></Layout>}/>
-        <Route path={"/login"} element={<Layout><Login/></Layout>}/>
-        <Route path={"/signup"} element={<Layout><SignUp/></Layout>}/>
+        <Route
+          path={"/"}
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path={"/login"}
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+        <Route
+          path={"/signup"}
+          element={
+            <Layout>
+              <SignUp />
+            </Layout>
+          }
+        />
       </Routes>
     </div>
   );
--- fontend/src/component/Home.jsx
+++ fontend/src/component/Home.jsx
@@ -1,7 +1,22 @@
 import AddToDo from "./todo/AddToDo";
 import AllToDo from "./todo/AllToDo";
+import { useNavigate } from "react-router-dom";
+import { useEffect} from "react";
+import { Auth } from "../services/Auth";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await Auth();
+      if(data !== 200) {
+        navigate("/login");
+      }
+      
+    };
+    fetchData();
+  },[navigate]);
   return (
     <div>
       <AddToDo />
